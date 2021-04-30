6313708ced45dfe2e8f39aa67e4acfcf674313b0
nimbly-dev <201801106@iacademy.edu.ph>
2021-04-30 12:55:46 +0800

    Done exercise 2.3 courseinfo step8

--- part2/courseinfo/src/components/course_components/TotalExercises.js
+++ part2/courseinfo/src/components/course_components/TotalExercises.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 const TotalExercises = ({ parts }) => {
+    //Done exercise 2.3* courseinformation step8 refactor as reduce
     const totalExercisesCount = parts.reduce((total, part) => part.exercises + total, 0);
     console.log(parts)
     return (

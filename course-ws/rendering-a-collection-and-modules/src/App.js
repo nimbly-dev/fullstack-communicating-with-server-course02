950efed7297d7069e40e1a2afbabb4dba8d070b7
nimbly-dev <nimbly2016@gmail.com>
2021-05-03 20:51:36 +0800

    Done exericse 2.6 and 2.7

---
+++ course-ws/rendering-a-collection-and-modules/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+//Import child components
+import Note from './components/Notes.js'
+
+const App = ({ notes }) => {
+  return (
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map(note =>
+          <Note key={note.id} note={note} />
+        )}
+      </ul>
+    </div>
+  )
+}
+
+export default App
